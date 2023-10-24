c7763969c905094737709c4a9c57ee271df3459e
Gaurav <gauravbisht154@gmail.com>
2023-10-24 14:56:40 +0530

    cookie options updated

--- controller/authController.js
+++ controller/authController.js
@@ -55,7 +55,7 @@ module.exports.signup_post = async (req, res, next) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "Lax",
+      sameSite: "None",
       maxAge: 3 * 24 * 60 * 60 * 1000,
     });
 
@@ -98,7 +98,7 @@ module.exports.login_post = async (req, res, next) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "Lax",
+      sameSite: "None",
       maxAge: 3 * 24 * 60 * 60 * 1000,
     });
 
@@ -113,7 +113,7 @@ module.exports.logout_get = (req, res, next) => {
   res.cookie("jwt", "", {
     httpOnly: true,
     secure: true,
-    sameSite: "Lax",
+    sameSite: "None",
     maxAge: 1,
   });
   res.json({ message: "logged out" });
