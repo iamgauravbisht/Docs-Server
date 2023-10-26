b5d04d9ee953234a4302798829d5430a60984a16
Gaurav <gauravbisht154@gmail.com>
2023-10-26 20:20:37 +0530

    cookie config updated

--- controller/authController.js
+++ controller/authController.js
@@ -55,7 +55,7 @@ module.exports.signup_post = async (req, res, next) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "None",
+      SameSite: "Lax",
       // domain: ".docserver-ecsy.onrender.com",
       // path: "/",
       maxAge: 3 * 24 * 60 * 60 * 1000,
@@ -100,7 +100,7 @@ module.exports.login_post = async (req, res, next) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "None",
+      SameSite: "Lax",
       // domain: ".docserver-ecsy.onrender.com",
       // path: "/",
       maxAge: 3 * 24 * 60 * 60 * 1000,
@@ -117,7 +117,7 @@ module.exports.logout_get = (req, res, next) => {
   res.cookie("jwt", "", {
     httpOnly: true,
     secure: true,
-    sameSite: "None",
+    SameSite: "Lax",
     // domain: ".docserver-ecsy.onrender.com",
     // path: "/",
     maxAge: 1,
